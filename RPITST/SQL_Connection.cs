6b57e9a010d92e7a54b0df8d05d63161432bd458
Saroeun Saing <guru.tech.kh@gmail.com>
2021-10-27 14:20:13 +0000

    Updated SQL_Connection.cs

--- RPITST/SQL_Connection.cs
+++ RPITST/SQL_Connection.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace RPITST
 {
     public abstract class SQL_Connection
     {
+        //Server Configuration
+        private  string datasource  = "GURUTECH";
+        private  string database = "loan";
+        private  string user_ID  = "sa";
+        private  string pwd  = "123";
+
+        private  string cs;
+
+
+        public Connect_SQLServer()
+        {
+            cs = "Data Source=" + datasource + ";Initial Catalog=" + database + "; User ID=" + user_ID + ";Password=" + pwd;
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(cs);
+        }
     }
 }
