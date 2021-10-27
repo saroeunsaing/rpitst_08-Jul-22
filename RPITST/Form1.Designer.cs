01577ab58a7643c0608de5fd47f3c62282aee074
GURUTECH <guru.tech.kh.com>
2021-10-27 20:56:53 +0700

    Second Commit

--- RPITST/Form1.Designer.cs
+++ RPITST/Form1.Designer.cs
@@ -29,24 +29,13 @@ namespace RPITST
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(69, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(243, 75);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,8 +43,6 @@ namespace RPITST
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
